147a4758ca45149dfa4d1d03d9a178b166563a55
JJ <teohjjteoh@gmail.com>
2024-12-23 13:01:43 +0800

    feat: add BlueSky link to navigation in app.vue for expanded social media integration

--- app/app.vue
+++ app/app.vue
@@ -16,15 +16,18 @@ const links = [
   ],
   [
     {
-      label: "Github",
       icon: "line-md:github-loop",
       to: "https://github.com/Ripwords",
       target: "_blank",
     },
     {
-      label: "x.com",
       icon: "line-md:twitter-x",
       to: "https://x.com/Ripwords_",
+      target: "_blank",
+    },
+    {
+      icon: "meteor-icons:bluesky",
+      to: "https://bsky.app/profile/ripwords.bsky.social",
       target: "_blank",
     },
   ],
