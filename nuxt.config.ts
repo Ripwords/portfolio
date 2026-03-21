90c8a48e6ffe7f66c9781a42353bdc1bfe19afdd
JJ <teohjjteoh@gmail.com>
2026-03-21 12:52:23 +0800

    chore: update compatibility version to 5 in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   future: {
-    compatibilityVersion: 4,
+    compatibilityVersion: 5,
   },
   experimental: {
     typedPages: true,
